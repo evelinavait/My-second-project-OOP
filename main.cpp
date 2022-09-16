1c0ca2fa9562cd0d397e4c529f38c1079eb435fa
Evelina <72965991+evelinavait@users.noreply.github.com>
2022-09-16 07:57:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,51 +1,58 @@
 #include "my_lib.hpp"
 
 struct irasas {
-string vardas;
-string pavarde;
-int paz[10];
-int egzas;
-double galut;
-double galut2;
-float median;
+  string vardas;
+  string pavarde;
+  int paz[10];
+  int egzas;
+  double galut = 0;
+  double galut2;
+  float median;
 };
 
-int main() {
+int main()
+{
   irasas temp;
-  vector<int> x;
+	int *nums;			// To dynamically allocate an array
+	int num_students;	// To hold the number of students
   int n = 0, sum = 0;
   int t;
-  
-  cout<<"Iveskite varda: "; cin>>temp.vardas;
-  cout<<"Iveskite pavarde: "; cin>>temp.pavarde;
-  cout <<"Iveskite egzamino paz.: "; cin>>temp.egzas;
-  /*cout <<"Kiek turi paz. (1-10)? "; cin>>n;*/
 
- /* for (int i = 0; i < n; i++){
+		// Prompt the user to enter how many students were.
+		cout << "Studentu skaicius: ";
+		cin >> num_students;
+
+
+		// Determine input validation.
+		while (num_students < 0)
+		{
+			cout << "Netinkamas skaicius!\n";
+			cout << "Studentu skaicius: ";
+			cin >> num_students;
+		}
+
+		// Dynamically allocate an array large enough to hold
+		// that many numbers of students.
+		nums = new int[num_students];
+
+		// Get the number of movies for each student;
+		for (int count = 0; count < num_students; count++)
+		{
+    cout << "Studento #" << (count + 1) << " informacija : \n";
+    //cin >> nums[count];
+    cout<<"Iveskite varda: "; cin>>temp.vardas;
+    cout<<"Iveskite pavarde: "; cin>>temp.pavarde;
+    cout <<"Iveskite egzamino paz.: "; cin>>temp.egzas;
+    cout <<"Kiek turi paz. (1-10)? "; cin>>n;
+
+   for (int i = 0; i < n; i++){
     cout<<"Iveskite "<<i+1<<"-aji paz.";
     cin>>temp.paz[i];
     sum+=temp.paz[i];
-    }*/
-
-  cout<<std::endl<<std::endl;
-  cout << "Suvedus reikiamus pazymius - iveskite endl"<<endl;
-  cout<<std::endl<<std::endl;
-  cout<<"Iveskite "<< n+1<<" -aji paz. ";
-    while ( cin>>temp.paz[n])
-   {
-     if (isdigit(temp.paz[n]) == false ){
-      cout<<"Iveskite "<<n+2<<" -aji paz. ";
-       x.push_back(temp.paz[n]);
-      sum +=temp.paz[n];
-      n +=1;}
-     else
-       cout<< " Pazymys ivestas    "<<endl;
-  }
-
-
-for (int i = 0; i < n; i++){
-  for (int j = 0; j < n - i - 1; j++)
-			if (temp.paz[j] < temp.paz[j + 1])
+    }
+      for (int i = 0; i < n; i++){
+        for (int j = 0; j < n - i - 1; j++)
+          if (temp.paz[j] < temp.paz[j + 1])
 			{
 				t = temp.paz[j];
 				temp.paz[j] = temp.paz[j + 1];
@@ -56,42 +63,53 @@ for (int i = 0; i < n; i++){
 	else
     temp.median = temp.paz[n/2];
     }
-
-  temp.galut = double(sum) / double(n)*0.4 + temp.egzas*0.6;
   temp.galut2 = temp.median*0.4 + temp.egzas*0.6;
 
-cout 
-  << setw(10)
-  << left 
-  << "Vardas"
-  << setw(10)
-  << left 
-  << "Pavarde"
-  << setw(19) 
-  << left 
-  << "Galutinis (Vid.) / "
-  << setw(10)
-  << left
-  << "Galutinis (Med.) \n"
+			// Determine input validation.
+			while (nums[count] < 0)
+			{
+				cout << "Netinkamas skaicius. Prasome ivesti teigiama skaiciu: ";
+				cout << "\nStudento #" << (count + 1) << " informacija : ";
+				cin >> nums[count];
+			}
+    }
+  // Set output formatting.
+		cout << fixed << showpoint << setprecision(1)
+  << setw(20) << left << "Vardas"
+  << setw(20) << left << "Pavarde"
+  << setw(10) << left << "Galutinis (Med.) \n"
   << "-----------------------------------------------------"
   << endl;
-
+//for (int count = 0; count < num_students; count++)
   {cout << fixed << setprecision(2)
-    << left
-    << setw(10)
-    << temp.vardas
-    << left
-    << setw(10)
-    << temp.pavarde
-    << left
-    << setw(19)
-    << temp.galut
-    << left
-    << setw(10)
-    << temp.galut2;}
+    << left << setw(20) << temp.vardas
+    << left << setw(20) << temp.pavarde
+    << left << setw(10) << temp.galut2;}
   cout << "\n\n";
-    
- /*cout << "Vektorius 'v' :";
-  for(int e : x){
-    cout <<e<<endl;}*/
+
+		// Free dynamically allocated memory.
+		delete[] nums;
+		nums = 0;		// Make nums point to null.
+
+
+
 }
+
+
+/*double calculateMedian(int *nums, int num_students)
+{
+	int middle;
+	double median;
+	middle = num_students / 2.0;
+
+	if (num_students % 2 == 0 )
+	{
+		median = nums[middle];
+	}
+	else
+	{
+		
+		median = (nums[middle] + nums[middle]) / 2.0;
+	}
+	return median; */
+  
