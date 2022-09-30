8f2ecbc19783b39bf95ed3d8356d20df0c0df8ca
Evelina <72965991+evelinavait@users.noreply.github.com>
2022-09-30 07:50:08 +0300

    Create my_lib2.hpp

---
+++ my_lib2.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include<stdio.h>
+#include<ctype.h>
+#include <bits/stdc++.h>
+#include <algorithm>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+using namespace std;
